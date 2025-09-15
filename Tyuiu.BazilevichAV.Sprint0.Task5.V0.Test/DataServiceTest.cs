6836ae00528a1ee2a4cb675efeaf66d24959f93d
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-09-15 19:44:43 +0500

    Task5

---
+++ Tyuiu.BazilevichAV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BazilevichAV.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.BazilevichAV.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChechedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+
+        public void ChechedRemovalValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+
+        public void ChechedSubtractionValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+
+        public void ChechedMultiplicationValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
