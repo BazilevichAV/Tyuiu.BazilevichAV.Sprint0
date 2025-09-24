58e47185e909765ecc1e1102d45aaefa40cbcf13
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-09-24 22:48:06 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BazilevichAV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BazilevichAV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BazilevichAV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алёна";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Алёна", res);
+        }
+    }
+}
---
+++ Tyuiu.BazilevichAV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.BazilevichAV.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Алёна"));
+Console.ReadKey();
